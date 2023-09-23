05234e0d9d97ac2203603e5ed63b155eb39fdae4
hadas <hadas@pagaya-inv.com>
2023-09-23 15:45:57 +0300

    Refactored code.

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -1,39 +1,86 @@
 # -*- coding: utf-8 -*-
+from enum import Enum
 
-class GildedRose(object):
+MAX_QUALITY = 50
+MIN_QUALITY = 0
+
+
+class ItemType(Enum):
+    AGED_BRIE = "Aged Brie"
+    BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert"
+    SULFURAS = "Sulfuras, Hand of Ragnaros"
+    DEFAULT = None
+
+    @classmethod
+    def _missing_(cls, value):
+        return cls.DEFAULT
+
+
+class ItemQualityUpdater:
+    @staticmethod
+    def _update_quality(sell_in: int, quality: int) -> int:
+        if sell_in >= 0:
+            return quality - 1
+
+        return quality - 2
+
+    @classmethod
+    def get_updated_quality(cls, sell_in: int, quality: int) -> int:
+        updated_quality = cls._update_quality(sell_in, quality)
+
+        if updated_quality >= MAX_QUALITY:
+            return MAX_QUALITY
+
+        if updated_quality < MIN_QUALITY:
+            return MIN_QUALITY
+
+        return updated_quality
+
+
+class BackstagePassesUpdater(ItemQualityUpdater):
+    @staticmethod
+    def _update_quality(sell_in: int, quality: int) -> int:
+        if sell_in == 0:
+            return 0
+        if sell_in <= 5:
+            return quality + 3
+        if sell_in <= 10:
+            return quality + 2
+
+        return quality + 1
+
+
+class AgedBrieUpdater(ItemQualityUpdater):
+    @staticmethod
+    def _update_quality(sell_in: int, quality: int) -> int:
+        return quality + 1
+
+
+class SulfurasUpdater(ItemQualityUpdater):
+    @classmethod
+    def get_updated_quality(cls, sell_in: int, quality: int) -> int:
+        return quality
+
+
+MAP_ITEM_TYPE_TO_QUALITY = {
+    ItemType.DEFAULT: ItemQualityUpdater,
+    ItemType.BACKSTAGE_PASSES: BackstagePassesUpdater,
+    ItemType.AGED_BRIE: AgedBrieUpdater,
+    ItemType.SULFURAS: SulfurasUpdater,
+}
+
+
+class GildedRose:
 
     def __init__(self, items):
         self.items = items
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+            item_type = ItemType(item.name)
+            item.quality = MAP_ITEM_TYPE_TO_QUALITY[item_type].get_updated_quality(
+                sell_in=item.sell_in, quality=item.quality
+            )
 
 
 class Item:
--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -1,16 +1,38 @@
 # -*- coding: utf-8 -*-
 import unittest
 
-from gilded_rose import Item, GildedRose
+from gilded_rose import Item, GildedRose, MIN_QUALITY, MAX_QUALITY
 
 
 class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
+    def test_minimal_quality(self):
+        items = [Item("foo", 0, MIN_QUALITY)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
+        self.assertEquals("foo", items[0].name)
+        self.assertEquals(MIN_QUALITY, items[0].quality)
 
-        
+    def test_maximal_quality(self):
+        items = [Item("Aged Brie", 5, MAX_QUALITY)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEquals("Aged Brie", items[0].name)
+        self.assertEquals(MAX_QUALITY, items[0].quality)
+
+    def test_decrease(self):
+        items = [Item("foo", 1, 10)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEquals("foo", items[0].name)
+        self.assertEquals(9, items[0].quality)
+
+    def test_aged_brie(self):
+        items = [Item("Aged Brie", 3, 12)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEquals("Aged Brie", items[0].name)
+        self.assertEquals(13, items[0].quality)
+
+
 if __name__ == '__main__':
     unittest.main()
